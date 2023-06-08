b7aae3e37e2b5073115fc66574670095306de40f
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-06-08 13:28:23 -0400

    Style: login partial

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,17 @@ const home = require('express').Router();
 
 home.get('/', async (req, res) => {
   try {
-    return res.status(200).render('home')
+    return res.status(200).render('home');
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+// Login Route
+home.get('/login', async (req, res) => {
+  try {
+    return res.status(200).render('login');
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: 'Server Error' });
